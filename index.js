76c743c941c7c381440e2e704e84cd391a36baf2
Laurence Bahiirwa <laurence@mediauganda.com>
2018-06-16 19:33:31 +0300

    Update index.js
    Issue is now at line 36

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const SlackBot = require('slackbots')
 const axios = require('axios')
 
 const words = {
-  please : 'bambi',
-  no: 'nedda'
+    please: 'bambi',
+    no: 'nedda'
 }
 
 const bot = new SlackBot({
-    token: 'xoxb-373454261911-382395365605-IUwpIHXTCRW5488P5Qdh0puYWPL',
+    token: 'xoxb-373454261911-382395365605-IUwpIHXTCRW5488P5Qdh0puY',
     'name': 'translate'
 });
 
 const channel = 'translateapp'
 
 //start Handler
-bot.on('start', ()=>{
+bot.on('start', () => {
     const params = {
         icon_emoji: ':smiley:'
     }
 
     bot.postMessageToChannel(
-        channel, 
+        channel,
         'Type @translate leave a space and the desired word then return to translate from English to Luganda.',
         params
     )
@@ -29,52 +29,26 @@ bot.on('start', ()=>{
 bot.on('error', (err) => console.log(err))
 
 bot.on('message', data => {
-    if(data.type !== 'message') {
+    if (data.type !== 'message') {
         return
     }
-    
+
+    // Issue is now here. How to run this evertime a new word is called.
     handleMessage(data.text)
 });
 
-// TODO: Improvement needs to come in at this stage. Look at code at bottom
-
-function handleMessage(message) {
-    if (message.includes(' please')) {
-        translateWord()
-    }
-}
-
-function translateWord() {
-    //const word = "bambi";
-    
-    const params = {
-        icon_emoji: ':smiley:'
-    }
-    
-    bot.postMessageToChannel( channel, `Please = ${words.please}`, params );
-}
- 
-/*
- * make an object of words to use as the dictionary
- const words = {
-  'please' : 'bambi',
-  'no': 'nedda'
- }
- 
- foreach ( words as word )
- {
-    bot.on('message', data => {
-        if(data.type !== 'message') {
-            return;
-        }
-
-        handleMessage(data.text);
-    });
-
+for (key in words){
     function handleMessage(message) {
-        if (message.includes(' please')) {
-            translateWord();
+        if (message.includes(' ' + key)) {
+            translateWord()
         }
     }
- }
-*/
+    
+    function translateWord() {
+        const params = {
+            icon_emoji: ':smiley:'
+        }
+    
+        bot.postMessageToChannel(channel, `${key} = ${words[key]}`, params);
+    }
+}
